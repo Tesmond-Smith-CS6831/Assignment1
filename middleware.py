3abc29573665ab9e798450d0170e6986eccd5ee4
Rick Tesmond <ricktesmond@Ricks-MBP.attlocal.net>
2021-02-03 14:25:04 -0600

    initialize files and included thoughts in new middleware.py file

---
+++ middleware.py
@@ -0,0 +1,11 @@
+"""
+Initial thoughts from RT:
+    - creating anonymity is the goal
+    - middleware could act as a hashtable with initialized buckets 'n' for number of zips
+        - key: zipcode
+        - value: weather stats
+    - Publisher script calls into middleware hashtable to update value using ziptable[zip] = collected_weather
+    - Subscriber script polls value from middleware using ziptable[zip] or ziptable.get(zip)
+
+    - this allows for neither the client/server knowing the pub/sub servers
+"""
